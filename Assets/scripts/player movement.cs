b1b9aa34eed5cf030c8dbffd26e9e6b22a8d43f2
emily-trinh <etrinh26@gmail.com>
2024-03-02 17:09:27 -0800

    Added idle + walking animations

--- Assets/scripts/player movement.cs
+++ Assets/scripts/player movement.cs
@@ -6,21 +6,40 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-
     private Rigidbody2D body;
+    private Animator anim;
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
+        
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
 
         if(Input.GetKey(KeyCode.Space)) {
             body.velocity = new Vector2(body.velocity.x, speed);
         }
+        
+        anim.SetBool("Walk", horizontalInput != 0);
+        flip();
+        
+
+    }  
+    
+    private void flip() {
+        float horizontalInputTwo = Input.GetAxis("Horizontal");
+
+        if ((horizontalInputTwo > 0) && (transform.localScale.x < 0)) {
+            transform.localScale = new Vector3((-1) * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        } else if ((horizontalInputTwo < 0) && (transform.localScale.x > 0)) {
+            transform.localScale = new Vector3((-1) * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
     }
+    
+        
 
 }
